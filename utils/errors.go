fe4e75d2cae14803e91b61e57a0974fd9c5e3eac
Alexis Trejo <marcoalexispt.02@gmail.com>
2024-09-28 16:55:43 -0600

    Add unathorized and validation errors

--- utils/errors.go
+++ utils/errors.go
@@ -3,5 +3,7 @@ package utils
 import "errors"
 
 var ErrNotFound = errors.New("not found")
+var ErrUnauthorized = errors.New("unauthorized")
 var ErrDatabase = errors.New("database error")
 var ErrValidation = errors.New("validation error")
+var ErrDuplicated = errors.New("validation error")
